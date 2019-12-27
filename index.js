e982ac63d6926463430de494f2230d2db4e6db35
Michael P <mpintos@lithium.com.uy>
2019-12-27 14:21:18 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+const lib = require("./lib");
+
+const sampleQuery = request => {
+  // here return data to client
+  return request.baseUrl;
+};
+
+const sampleMutator = request => {
+  // here insert data to database
+  return request.baseUrl;
+};
+
+const schema = {
+  // name of method is for subscribe on client -> lib.subscribe('method_name', query, callback);
+  user: {
+    queries: {
+      "/": sampleQuery,
+      "/admins": sampleQuery
+    },
+    mutators: {
+      // if is emty use polling method
+      "/": {
+        method: "post",
+        handler: sampleMutator
+      },
+      "/admin": {
+        method: "put",
+        handler: sampleMutator
+      }
+    }
+  }
+};
+
+// Init lib
+lib(app, schema);
+
+app.listen("3000");
