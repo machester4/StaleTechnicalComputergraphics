43aa81265d5923b982695615088cfbe0767d3b07
Michael P <mpintos@lithium.com.uy>
2019-12-27 14:22:34 -0300

    WIP

--- index.js
+++ index.js
@@ -15,10 +15,7 @@ const sampleMutator = request => {
 const schema = {
   // name of method is for subscribe on client -> lib.subscribe('method_name', query, callback);
   user: {
-    queries: {
-      "/": sampleQuery,
-      "/admins": sampleQuery
-    },
+    queries: [{path: "/", handler: sampleQuery, }]
     mutators: {
       // if is emty use polling method
       "/": {
